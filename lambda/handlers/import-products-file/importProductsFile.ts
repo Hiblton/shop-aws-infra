b3bed3bd33eb669985f6c2da5933e042563b1fea
Slava Selivonchyk <slava.selivonchyk@gmail.com>
2024-11-02 23:55:40 +0100

    Task 5.2: add lambda to generate Signed Url and upload csv file to S3 bucket

---
+++ lambda/handlers/import-products-file/importProductsFile.ts
@@ -0,0 +1,54 @@
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+const headers = {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Origin": "https://d393tsl9iif6hb.cloudfront.net",
+    "Access-Control-Allow-Credentials": "true",
+    "Access-Control-Allow-Methods": "OPTIONS,GET"
+};
+
+const s3 = new S3Client();
+
+export const handler: APIGatewayProxyHandler = async (event) => {
+    const fileName = event.queryStringParameters?.name;
+    if (!fileName) {
+        return {
+            statusCode: 400,
+            headers,
+            body: JSON.stringify({ error: "File name is required as a query parameter." })
+        };
+    }
+
+    const fileExt = fileName.split('.').pop();
+    if (fileExt !== 'csv') {
+        return {
+            statusCode: 400,
+            headers,
+            body: JSON.stringify({ error: "Only csv file is allowed." })
+        };
+    }
+
+    const params = {
+        Bucket: process.env.BUCKET_NAME,
+        Key: `uploaded/${fileName}`
+    };
+
+    try {
+        const url = await getSignedUrl(s3, new PutObjectCommand(params), { expiresIn: 3600 });
+        return {
+            statusCode: 200,
+            headers,
+            body: JSON.stringify({ url })
+        };
+    } catch (err) {
+        console.error('Error creating signed URL:', err);
+        return {
+            statusCode: 500,
+            headers,
+            body: JSON.stringify({ error: "Error creating signed URL" })
+        };
+    }
+};
--- lib/import-service-stack.ts
+++ lib/import-service-stack.ts
@@ -1,20 +1,76 @@
-import { aws_s3, aws_s3_deployment, CfnOutput, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { CfnOutput, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as path from 'path';
+
+const CORS_CONFIGURATION = {
+    allowOrigins: ['https://d393tsl9iif6hb.cloudfront.net'],
+    allowMethods: ['OPTIONS', 'GET'],
+    allowHeaders: ['Content-Type', 'Authorization'],
+}
 
 export class ImportServiceStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
-        const importBucket = new aws_s3.Bucket(this, "ImportBucket", {
+        const importBucket = new s3.Bucket(this, "ImportBucket", {
             autoDeleteObjects: true,
-            blockPublicAccess: aws_s3.BlockPublicAccess.BLOCK_ALL,
+            blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
             removalPolicy: RemovalPolicy.DESTROY,
+            cors: [
+                {
+                    allowedOrigins: CORS_CONFIGURATION.allowOrigins,
+                    allowedMethods: [s3.HttpMethods.PUT],
+                },
+            ],
         })
 
-        new aws_s3_deployment.BucketDeployment(this, 'ImportBucketDeployment', {
-            sources: [aws_s3_deployment.Source.asset('./assets')],
+        new s3deploy.BucketDeployment(this, 'ImportBucketDeployment', {
+            sources: [s3deploy.Source.asset('./assets')],
             destinationBucket: importBucket,
             destinationKeyPrefix: 'uploaded/'
+        });
+
+        const lambdaRole = new iam.Role(this, 'LambdaExecutionRole', {
+            assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+            description: 'Role for Import Products Lambda Function',
+        });
+
+        lambdaRole.addToPolicy(new iam.PolicyStatement({
+            effect: iam.Effect.ALLOW,
+            actions: ['s3:GetObject', 's3:PutObject'],
+            resources: [importBucket.bucketArn, `${importBucket.bucketArn}/*`],
+        }));
+
+        const importProductsFile = new lambda.Function(this, 'ImportProductsFile', {
+            runtime: lambda.Runtime.NODEJS_20_X,
+            memorySize: 128,
+            handler: 'importProductsFile.handler',
+            code: lambda.Code.fromAsset(path.join(__dirname, '../lambda/handlers/import-products-file')),
+            role: lambdaRole,
+            environment: {
+                BUCKET_NAME: importBucket.bucketName
+            }
+        });
+
+        importBucket.grantPut(importProductsFile);
+
+        const api = new apigateway.RestApi(this, 'ImportServiceApi', {
+            restApiName: 'Import Service API'
+        });
+
+        const importResource = api.root.addResource('import');
+        importResource.addMethod('GET', new apigateway.LambdaIntegration(importProductsFile));
+        importResource.addCorsPreflight(CORS_CONFIGURATION);
+
+        new CfnOutput(this, "APIGatewayURL", {
+            value: api.url,
+            description: "URL of the API Gateway",
+            exportName: "ImportAPIURL"
         });
 
         new CfnOutput(this, "ImportBucketName", {
